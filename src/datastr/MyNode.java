a72d4312226da09a8b7318007b22b4c2fa020df3
Ritvars <ritvarsbaders@gmail.com>
2026-03-31 11:04:42 +0300

    Add: added myNodeClass

--- src/datastr/MyNode.java
+++ src/datastr/MyNode.java
@@ -1,6 +1,43 @@
 package datastr;
 
-public class MyNode {
+public class MyNode<Ttype> {
+	private Ttype element;
+	private MyNode nextNode = null;
+	private MyNode previousNode = null;
 	
+	public Ttype getElement() {
+		return element;
+	}
+	
+	public MyNode getNextNode() {
+		return nextNode;
+	}
+	public MyNode getPreviousNode() {
+		return previousNode;
+	}
+	
+	public void setElement(Ttype newElement) {
+		if (!newElement.equals(null)) {
+			element = newElement;	
+		}
+		else {
+			element = (Ttype)new Object();
+		}
+		
+	}
+	public void setNextNode(MyNode newNextNode) {
+		nextNode = newNextNode;
+	}
+	
+	public void setPreviousNode(MyNode newPreviousNode) {
+		previousNode = newPreviousNode;
+	}
+	
+	public MyNode(Ttype newElement) {
+		setElement(newElement);
+	}
+	public String toString() {
+		return "" + element;
+	}
 	
 }
